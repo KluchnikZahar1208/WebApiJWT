f4889a8cfeca2eeecaf5db2cd276bca25a634842
Kluchnik Zahar <kluchnikzahar1208@gmail.com>
2023-02-09 04:06:40 +0300

    jwtToken

---
+++ WebApiJWT/Models/User.cs
@@ -0,0 +1,14 @@
+using System.Security.Cryptography;
+using System;
+namespace WebApiJWT.Models
+{
+    public class User
+    {
+        public string UserName {get; set; } = string.Empty;
+        public byte[]? PasswordHash {get; set;}
+        public byte[]? PasswordSalt {get; set;}
+        public string RefreshToken {get; set;} = string.Empty;
+        public DateTime TokenCreated {get;set;}
+        public DateTime TokenExpires {get;set;}
+    }
+}
